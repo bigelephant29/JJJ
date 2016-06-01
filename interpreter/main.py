296efba3a04d703214e77bb9ff69aae371451abf
bigelephant29 <dc9491@gmail.com>
2016-06-01 17:03:54 +0800

    put all function and variable into a class called JJJInterpreter, add buff queue and rename some functions

--- interpreter/main.py
+++ interpreter/main.py
@@ -1,129 +1,150 @@
-'''
-Command List:
-up
-down
-left
-right
-jump
-label
-$1~$4
-+ - * /
-print=$1~$4 (command for debugging)
-'''
+from enum import Enum
+import queue
 
-global register
-register = [-999, 0, 0, 0, 0]
-
-# Function for parsing a single element (not a expression)
-def getElementValue(exp):
-
-    # Case for register element
-    if exp[0] == '$':
-        if len(exp) != 2:
+class JJJInterpreter:
+     
+    def __init__(self):
+        self.isLabeled = 0
+        self.commandList = []
+        self.register = [-999, 0, 0, 0, 0]   
+        self.buffQueue = queue.Queue()
+        
+    # Enum Class for command types
+    class CommandType(Enum):
+        noType = 0
+        up = 1
+        down = 2
+        left = 3
+        right = 4
+        jump = 5
+        label = 6
+        assign = 7
+        debug_print = 999
+     
+    # Function for parsing a single element (not a expression)
+    def getElementValue(self, exp):
+        # Case for self.register element
+        if exp[0] == '$':
+            if len(exp) != 2:
+                return None
+            if exp[1] not in ['1', '2', '3', '4']:
+                return None
+            return self.register[ord(exp[1]) - ord('0')]
+        # Case for constant element
+        try:
+            val = int(exp)
+        except ValueError:
             return None
-        if exp[1] not in ['1', '2', '3', '4']:
+        return val
+        
+    # Function for evaluating the value of a expression
+    def parseRightExpr(self, exp):
+        operatorList = ["+", "-", "*", "/"]
+        operatorType = None
+        operatorIndex = 0
+        # Check if the expression contains an operator in operator list
+        for i in operatorList:
+            operatorIndex = exp.rfind(i)
+            if operatorIndex != -1:
+                operatorType = i
+                break
+        # Deal with the leading '-' for some constant expressions
+        if operatorType == '-' and operatorIndex == 0:
+            operatorType = None;
+        # Case for constant expression
+        if operatorType == None:
+            return self.getElementValue(exp)
+        # Case for non-constant expression
+        expressionElement = exp.split(operatorType)
+        val1 = self.getElementValue(expressionElement[0])
+        val2 = self.getElementValue(expressionElement[1])
+        if val1 == None or val2 == None:
             return None
-        return register[ord(exp[1]) - ord('0')]
-
-    # Case for constant element
-    try:
-        val = int(exp)
-    except ValueError:
-        return None
-    return val
-
-# Function for evaluating the value of a expression
-def parseExpression(exp):
-
-    operatorList = ["+", "-", "*", "/"]
-    operatorType = None
-    operatorIndex = 0
-    # Check if the expression contains an operator in operator list
-    for i in operatorList:
-        operatorIndex = exp.rfind(i)
-        if operatorIndex != -1:
-            operatorType = i
-            break
-
-    # Deal with the leading '-' for some constant expressions
-    if operatorType == '-' and operatorIndex == 0:
-        operatorType = None;
-
-    # Case for constant expression
-    if operatorType == None:
-        return getElementValue(exp)
-
-    # Case for non-constant expression
-    expressionElement = exp.split(operatorType)
-    val1 = getElementValue(expressionElement[0])
-    val2 = getElementValue(expressionElement[1])
-    if val1 == None or val2 == None:
-        return None
-    if operatorType == "+":
-        return val1 + val2
-    elif operatorType == "-":
-        return val1 - val2
-    elif operatorType == "*":
-        return val1 * val2
-    elif operatorType == "/":
-        return val1 / val2
-
-# Function for sending command to interpreter
-def interpreterSend(cmd):
-
-    # Remove spaces in command
-    cmd = cmd.replace(" ", "")
-
-    # Try to find "=" in command
-    equalLocation = cmd.find("=")
-
-    # equalLocation == -1  means the command is not a assignment
-    if equalLocation == -1:
-
-        # Command Types
-        if cmd == "up":
-            pass
-        elif cmd == "down":
-            pass
-        elif cmd == "left":
-            pass
-        elif cmd == "right":
-            pass
-        elif cmd == "jump":
-            pass
-        elif cmd == "label":
-            pass
-        else:
-            print '[ERROR] Invalid Input'
-
-    else:
-
-        # Assign/Operation Type
-        cmd = cmd.split('=')
-
-        # print=$1 command is for debugging
-        if cmd[0] == 'print':
-            if len(cmd[1]) == 2 and cmd[1][0] == '$' and cmd[1][1] in ['1', '2', '3', '4']:
-                print 'value of ' + cmd[1] + ' = ' + str(register[ord(cmd[1][1])-ord('0')]);
-        elif cmd[0][0] == '$':
-            if cmd[0][1] in ['1', '2', '3', '4']:
-                # Parse the assignment
-                regNum = ord(cmd[0][1]) - ord('0')
-                expressionVal = parseExpression(cmd[1])
-                # If there are some mistakes in the expression, expressionVal will be None
-                if expressionVal != None:
-                    register[regNum] = expressionVal
-                else:
-                    print '[ERROR] Invalid Input'
+        if operatorType == "+":
+            return val1 + val2
+        elif operatorType == "-":
+            return val1 - val2
+        elif operatorType == "*":
+            return val1 * val2
+        elif operatorType == "/":
+            return val1 / val2
+            
+    # Function for sending command to interpreter, return 1 on success, 0 on fail
+    def addCommand(self, cmd):
+        # Remove spaces in command
+        cmd = cmd.replace(" ", "")
+        # Try to find "=" in command
+        equalLocation = cmd.find("=")
+        # equalLocation == -1  means the command is not a assignment
+        if equalLocation == -1:
+            # Command Types
+            if cmd == "up":
+                if self.isLabeled == 1:
+                    self.commandList.append((self.CommandType.up,));
+            elif cmd == "down":
+                if self.isLabeled == 1:
+                    self.commandList.append((self.CommandType.down,));
+            elif cmd == "left":
+                if self.isLabeled == 1:
+                    self.commandList.append((self.CommandType.left,));
+            elif cmd == "right":
+                if self.isLabeled == 1:
+                    self.commandList.append((self.CommandType.right,));
+            elif cmd == "jump":
+                if self.isLabeled == 1:
+                    self.commandList.append((self.CommandType.jump,));
+            elif cmd == "label":
+                print(len(self.commandList))
+                # Set a label for jump
+                if ( len(self.commandList) > 0 ):
+                    del self.commandList[:]
+                # Set labeled flag
+                self.isLabeled = 1
             else:
-                print '[ERROR] Invalid Input'
+                print ('[ERROR] Invalid Input: ', cmd)
+                return 0
         else:
-            print '[ERROR] Invalid Input'
+            # Assign/Operation Type
+            nowCommand = (self.CommandType.assign, cmd)
+            cmd = cmd.split('=')
+            # print=$1 command is for debugging
+            if cmd[0] == 'print':
+                if len(cmd[1]) == 2 and cmd[1][0] == '$' and cmd[1][1] in ['1', '2', '3', '4']:
+                    print ('value of ' + cmd[1] + ' = ' + str(self.register[ord(cmd[1][1])-ord('0')]))
+            elif cmd[0][0] == '$':
+                if cmd[0][1] in ['1', '2', '3', '4']:
+                    # Parse the assignment
+                    regNum = ord(cmd[0][1]) - ord('0')
+                    expressionVal = self.parseRightExpr(cmd[1])
+                    # If there are some mistakes in the expression, expressionVal will be None
+                    if expressionVal != None:
+                        # Valid expression
+                        self.register[regNum] = expressionVal
+                        self.commandList.append(nowCommand)
+                    else:
+                        print ('[ERROR] Invalid Input: ', nowCommand[1])
+                        return 0
+                else:
+                    print ('[ERROR] Invalid Input: ', nowCommand[1])
+                    return 0
+            else:
+                print ('[ERROR] Invalid Input: ', nowCommand[1])
+                return 0
+        return 1
 
+    # Function for getting a command from buffer
+    def getCommand(self):
+        if self.buffQueue.empty:
+            print('[ERROR] No buffed command')
+        else:
+            return self.buffQueue.get()
+        
 def main():
+    inte = JJJInterpreter()
     while True:
-        str = raw_input()
-        interpreterSend(str)
-
+        inputString = input()
+        inte.addCommand(inputString)
+        print(inte.getCommand())
+        
 if __name__ == "__main__":
     main()
